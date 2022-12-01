54bd81167a2a7eaf7c1fa2239a41dd15d5e54a17
Nathan Joudet <nathanjoudet@orange.fr>
2022-12-01 16:56:54 +0100

    feat: Continuation  :construction:

--- DiceGame/Classes/Partie.ts
+++ DiceGame/Classes/Partie.ts
@@ -1,58 +1,53 @@
-// import Gobelet from "./Gobelet";
-// import Joueur from "./Joueur";
+import Gobelet from "./Gobelet";
+import Joueur from "./Joueur";
 
-// export default class Partie extends Joueur {
-//     public nbTours: number;
-
-//     constructor(score: number, nom: string, nbTours: number) {
-//         super(score, nom)
-//         this.nbTours = nbTours;
-//     }
-
-//     /**
-//      * Intancier le nombre de dés nécéssaires
-//      */
-//     public preparerPartie() {
-
-//     }
-
-//     public initialiserPartie() { // : void
-
-//     }
-
-    
-//     public lancerPartie() { // : number
-
-//     }
-
-//     /**
-//      * Lancer les dés de chaque joueur
-//      */
-//     public jouerTour() { 
-
-//     }
-
-//     /**
-//      * Calcul le score
-//      */
-//     public calculerScore() {
-
-//     }
-
-//     /**
-//      * Déterminer qui à gagné la manche
-//      */
-//     public definirVainqueur() { // : string
-
-//     }
+export default class Partie {
+    public nbTours: number;
+    public joueursDansLaPartie: Joueur[] = [];
+    public gobelet: Gobelet = new Gobelet(6);
 
 
-//     public afficherGagnant() { // : string
+    constructor(nbTours: number) {
+        this.nbTours = nbTours;
+    }
 
-//     }
+    /**
+     * Initialiser la partie
+     * Nb de Joueurs
+     * Nb de Dés
+     * Nb de tours
+     * Gobelet
+     */
+
+
+    public initialiserPartie(...joueursEntrant: Joueur[]) {
+        joueursEntrant.forEach(element => {
+            this.joueursDansLaPartie.push(element);
+            this.gobelet.ajouteDe(6);
+        });
+
+
+    }
+
+    /**
+     * Lancer la partie
+     * 
+     */
+    public lancerPartie() { // : number
+        // lance les dés
+        // 
+
+    }
+
+    /**
+     * Afficher le gagnant
+     */
+    public afficherGagnant() { // : string
+
+    }
 
 
 
 
 
-// }
+}
